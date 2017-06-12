09c95581144fa4d8e83ff7c3a53af29af284ac0e
collins <collomash4@gmail.com>
2017-06-12 19:58:46 +0300

    test if the input is an integer

--- test_prime.py
+++ test_prime.py
@@ -5,7 +5,10 @@ from prime_number import getPrime
 def test_output_positive_numers_only(self):
     getResult=getPrime(10)
     for i in getResult:
-        self.assertTrue(i>0)                
+        self.assertTrue(i>0)   
+def test_if_value_is_integer(self):
+        with self.assertRaises(TypeError):
+            getPrime('success')                     
 if  __name__=='__main__':
     unittest.main()
 
